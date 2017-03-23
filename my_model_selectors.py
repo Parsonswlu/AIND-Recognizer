65d48f2119e723b639711abf6b8c9442ac790cbc
Parsons <atparsons@gmail.com>
2017-03-23 08:27:46 -0400

    initial commit

--- my_model_selectors.py
+++ my_model_selectors.py
@@ -38,8 +38,10 @@ class ModelSelector(object):
         try:
             hmm_model = GaussianHMM(n_components=num_states, covariance_type="diag", n_iter=1000,
                                     random_state=self.random_state, verbose=False).fit(self.X, self.lengths)
+            # logL = self.score(self.X,self.lengths)
             if self.verbose:
                 print("model created for {} with {} states".format(self.this_word, num_states))
+                # print("model created for {} with {} states and logL = {}".format(self.this_word, num_states, logL))                
             return hmm_model
         except:
             if self.verbose:
@@ -77,7 +79,30 @@ class SelectorBIC(ModelSelector):
         warnings.filterwarnings("ignore", category=DeprecationWarning)
 
         # TODO implement model selection based on BIC scores
-        raise NotImplementedError
+        word_sequences = self.sequences
+        n=self.min_n_components
+        BIC = {}        
+        while (n <= self.max_n_components):
+            # Use try/except construct to catch ValueErrors
+            try:          
+                # train a model based on current number of components
+                model = GaussianHMM(n_components=n, covariance_type="diag", n_iter=1000,
+                                        random_state=self.random_state, verbose=False).fit(self.X, self.lengths)
+                # calculate log loss for the model
+                logL = model.score(self.X, self.lengths)
+                # Calculate number of free parameters and log of the number of examples
+                p = n*(n-1)
+                logN = np.log(len(self.X))
+                # Calculate BIC score using provided calculation and above model parameters
+                BIC[n] = -2 * logL + p*logN
+            except ValueError:
+                pass
+            n+=1
+
+        # determine the number of components with the minimum BIC score
+        best_num_components = min(BIC,key=BIC.get)
+        # return best model
+        return self.base_model(best_num_components)
 
 
 class SelectorDIC(ModelSelector):
@@ -93,7 +118,33 @@ class SelectorDIC(ModelSelector):
         warnings.filterwarnings("ignore", category=DeprecationWarning)
 
         # TODO implement model selection based on DIC scores
-        raise NotImplementedError
+        word_sequences = self.sequences
+        n=self.min_n_components
+        logProbX = {}
+        DIC = {}        
+        while (n <= self.max_n_components):
+            # Use try/except construct to catch ValueErrors
+            try:          
+                # train a model based on current number of components
+                model = GaussianHMM(n_components=n, covariance_type="diag", n_iter=1000,
+                                        random_state=self.random_state, verbose=False).fit(self.X, self.lengths)
+                # calculate log probability for the model
+                logProbX[n] = model.score(self.X, self.lengths)
+            except ValueError:
+                pass
+            n+=1
+
+        # Create a list of each of the choices (n_choices) and how many their are (M)        
+        n_choices = list(logProbX.keys())
+        M = len(n_choices)
+        # Calculate DIC score using provided calculation and above model parameters        
+        for i in n_choices:
+            DIC[i] = logProbX[i] - (1/(M-1))*sum([logProbX[j] for j in n_choices if i!=j])
+
+        # determine the number of components with the minimum DIC score
+        best_num_components = min(DIC,key=DIC.get)
+        # return best model
+        return self.base_model(best_num_components)
 
 
 class SelectorCV(ModelSelector):
@@ -103,6 +154,48 @@ class SelectorCV(ModelSelector):
 
     def select(self):
         warnings.filterwarnings("ignore", category=DeprecationWarning)
+        warnings.filterwarnings("ignore", category=RuntimeWarning)        
 
         # TODO implement model selection using CV
-        raise NotImplementedError
+        word_sequences = self.sequences
+        n=self.min_n_components        
+        split_method = KFold(min(3,len(word_sequences))) 
+        avgLogL={}
+        while (n <= self.max_n_components):
+            # Use try/except construct to catch ValueErrors
+            try:
+                sum_logL=0
+                numCrossVals=0                
+                # split word_sequences into training and testing sets for k folds
+                for cv_train_idx, cv_test_idx in split_method.split(word_sequences):
+                    X_train, lengths_train = combine_sequences(cv_train_idx, word_sequences)
+                    X_test, lengths_test = combine_sequences(cv_test_idx, word_sequences)
+                    # train a submodel based on current train/test split
+                    submodel = GaussianHMM(n_components=n, covariance_type="diag", n_iter=1000,
+                                            random_state=self.random_state, verbose=False).fit(X_train, lengths_train)
+                    # calculate log loss for current submodel
+                    logL = submodel.score(X_test,lengths_test)
+                    # add log loss to cumulative log loss for this number of components and increment counter
+                    sum_logL += logL
+                    numCrossVals += 1
+                # Calculate average log loss for the k folds                
+                avgLogL[n] = (sum_logL / numCrossVals)
+                # print("avgLogLoss for %d components is %d" % (n,avgLogL[n]))
+            except ValueError:
+                pass
+            n+=1
+
+        # determine the number of components with the minimum log loss and return it
+        best_num_components = min(avgLogL,key=avgLogL.get)
+        # return model with optimal number of components
+        return self.base_model(best_num_components)        
+
+# For testing purposes
+if __name__ == "__main__":
+    from  asl_test_model_selectors import TestSelectors
+    test_model = TestSelectors()
+    test_model.setUp()
+    test_model.test_select_constant_interface()
+    test_model.test_select_cv_interface()
+    test_model.test_select_bic_interface()
+    test_model.test_select_dic_interface()
--- my_recognizer.py
+++ my_recognizer.py
@@ -5,10 +5,10 @@ from asl_data import SinglesData
 def recognize(models: dict, test_set: SinglesData):
     """ Recognize test word sequences from word models set
 
-   :param models: dict of trained models
+    :param models: dict of trained models
        {'SOMEWORD': GaussianHMM model object, 'SOMEOTHERWORD': GaussianHMM model object, ...}
-   :param test_set: SinglesData object
-   :return: (list, list)  as probabilities, guesses
+    :param test_set: SinglesData object
+    :return: (list, list)  as probabilities, guesses
        both lists are ordered by the test set word_id
        probabilities is a list of dictionaries where each key a word and value is Log Liklihood
            [{SOMEWORD': LogLvalue, 'SOMEOTHERWORD' LogLvalue, ... },
@@ -16,10 +16,28 @@ def recognize(models: dict, test_set: SinglesData):
             ]
        guesses is a list of the best guess words ordered by the test set word_id
            ['WORDGUESS0', 'WORDGUESS1', 'WORDGUESS2',...]
-   """
+    """
     warnings.filterwarnings("ignore", category=DeprecationWarning)
+    warnings.filterwarnings("ignore", category=RuntimeWarning)
     probabilities = []
     guesses = []
     # TODO implement the recognizer
-    # return probabilities, guesses
-    raise NotImplementedError
+    for this_word in test_set.get_all_sequences():
+        X_test, lengths_test = test_set.get_item_Xlengths(this_word)
+        logL = {}
+        for this_key, this_model in models.items():
+            try:
+                logL[this_key] = this_model.score(X_test,lengths_test)
+            except ValueError:
+                logL[this_key] = float("inf")
+        probabilities.append(logL)
+        guesses.append(min(logL,key=logL.get))
+    return probabilities, guesses
+
+# For testing purposes
+if __name__ == "__main__":
+    from  asl_test_recognizer import TestRecognize
+    test_model = TestRecognize()
+    test_model.setUp()
+    test_model.test_recognize_probabilities_interface()
+    test_model.test_recognize_guesses_interface()
